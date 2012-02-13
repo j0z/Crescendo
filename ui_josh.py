491ee9cdf031eb93208d888ba91a0be2255261a9
unknown <ir8llama@gmail.com>
2012-02-13 15:25:38 -0500

    Metro is now updated!

--- ui_josh.py
+++ ui_josh.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui\main_josh.ui'
+# Form implementation generated from reading ui file 'ui/main_josh.ui'
 #
-# Created: Tue Feb 07 22:50:28 2012
+# Created: Mon Feb 13 15:23:51 2012
 #      by: PyQt4 UI code generator 4.9
 #
 # WARNING! All changes made in this file will be lost!
@@ -124,7 +124,7 @@ class Ui_MainWindow(object):
 "}"))
         self.label.setObjectName(_fromUtf8("label"))
         self.lst_files = QtGui.QTreeWidget(self.centralwidget)
-        self.lst_files.setGeometry(QtCore.QRect(129, 38, 647, 340))
+        self.lst_files.setGeometry(QtCore.QRect(129, 38, 647, 316))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -386,7 +386,7 @@ class Ui_MainWindow(object):
         self.lst_nodes.setLineWidth(0)
         self.lst_nodes.setObjectName(_fromUtf8("lst_nodes"))
         self.btn_grab = QtGui.QPushButton(self.centralwidget)
-        self.btn_grab.setGeometry(QtCore.QRect(653, 379, 117, 29))
+        self.btn_grab.setGeometry(QtCore.QRect(657, 355, 117, 22))
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(26, 26, 26))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -421,7 +421,7 @@ class Ui_MainWindow(object):
         self.btn_grab.setFlat(True)
         self.btn_grab.setObjectName(_fromUtf8("btn_grab"))
         self.btn_connect = QtGui.QPushButton(self.centralwidget)
-        self.btn_connect.setGeometry(QtCore.QRect(256, 377, 89, 29))
+        self.btn_connect.setGeometry(QtCore.QRect(693, 2, 105, 26))
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(26, 26, 26))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -452,22 +452,26 @@ class Ui_MainWindow(object):
 "    background-color:rgb(176,176,176);\n"
 "\n"
 "}"))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../gfx/gear.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btn_connect.setIcon(icon)
+        self.btn_connect.setIconSize(QtCore.QSize(16, 16))
         self.btn_connect.setDefault(True)
         self.btn_connect.setFlat(True)
         self.btn_connect.setObjectName(_fromUtf8("btn_connect"))
-        self.lne_ip = QtGui.QLineEdit(self.centralwidget)
-        self.lne_ip.setGeometry(QtCore.QRect(1, 383, 256, 20))
+        self.lne_filter = QtGui.QLineEdit(self.centralwidget)
+        self.lne_filter.setGeometry(QtCore.QRect(129, 359, 256, 20))
         font = QtGui.QFont()
         font.setPointSize(12)
-        self.lne_ip.setFont(font)
-        self.lne_ip.setStyleSheet(_fromUtf8("QLineEdit{\n"
+        self.lne_filter.setFont(font)
+        self.lne_filter.setStyleSheet(_fromUtf8("QLineEdit{\n"
 "    border: 2px solid rgb(121,121,121);\n"
 "    selection-background-color: rgb(176,176,176);\n"
 "    selection-color: rgb(26,26,26);\n"
 "}"))
-        self.lne_ip.setObjectName(_fromUtf8("lne_ip"))
+        self.lne_filter.setObjectName(_fromUtf8("lne_filter"))
         self.tabWidget = QtGui.QTabWidget(self.centralwidget)
-        self.tabWidget.setGeometry(QtCore.QRect(2, 407, 801, 175))
+        self.tabWidget.setGeometry(QtCore.QRect(2, 383, 801, 199))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Segoe UI"))
         font.setPointSize(18)
@@ -499,6 +503,7 @@ class Ui_MainWindow(object):
 "    margin-top: 5px; /* make non-selected tabs look smaller */\n"
 "    color: rgb(76,76,76);\n"
 " }"))
+        self.tabWidget.setTabPosition(QtGui.QTabWidget.North)
         self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
         self.tab = QtGui.QWidget()
         self.tab.setObjectName(_fromUtf8("tab"))
@@ -661,10 +666,128 @@ class Ui_MainWindow(object):
         self.lst_log.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
         self.lst_log.setObjectName(_fromUtf8("lst_log"))
         self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_4 = QtGui.QWidget()
+        self.tab_4.setObjectName(_fromUtf8("tab_4"))
+        self.txt_news = QtGui.QTextBrowser(self.tab_4)
+        self.txt_news.setGeometry(QtCore.QRect(-2, -1, 798, 131))
+        self.txt_news.setStyleSheet(_fromUtf8("QScrollBar:vertical {\n"
+"    border: none;\n"
+"    background: rgba(0,0,0,0);\n"
+"    width: 10px;\n"
+"    margin: 10px 0px 10px 0px;\n"
+"}\n"
+"QScrollBar::handle:vertical {\n"
+"    background: rgb(121,121,121);\n"
+"    min-height: 20px;\n"
+"}\n"
+"QScrollBar::handle:vertical:hover {\n"
+"    background: rgb(26,26,26);\n"
+"    min-height: 10px;\n"
+"}\n"
+"QScrollBar::add-line:vertical {\n"
+"    background: white;\n"
+"    height: 20px;\n"
+"    subcontrol-position: bottom;\n"
+"    subcontrol-origin: margin;\n"
+"}\n"
+"QScrollBar::sub-line:vertical {\n"
+"    background: white;\n"
+"    height: 10px;\n"
+"    subcontrol-position: top;\n"
+"    subcontrol-origin: margin;\n"
+"}\n"
+"\n"
+"\n"
+"\n"
+"\n"
+"\n"
+"QScrollBar:horizontal {\n"
+"    border: none;\n"
+"    background: rgba(0,0,0,0);\n"
+"    width: 10px;\n"
+"    margin: 10px 0px 20px 0px;\n"
+"}\n"
+"QScrollBar::handle:horizontal {\n"
+"    background: rgb(121,121,121);\n"
+"    min-height: 10px;\n"
+"}\n"
+"QScrollBar::handle:horizontal:hover {\n"
+"    background: rgb(26,26,26);\n"
+"    min-height: 10px;\n"
+"}\n"
+"QScrollBar::add-line:horizontal {\n"
+"    background: white;\n"
+"    height: 10px;\n"
+"    subcontrol-position: bottom;\n"
+"    subcontrol-origin: margin;\n"
+"}\n"
+"QScrollBar::sub-line:horizontal {\n"
+"    background: white;\n"
+"    height: 20px;\n"
+"    subcontrol-position: top;\n"
+"    subcontrol-origin: margin;\n"
+"}"))
+        self.txt_news.setObjectName(_fromUtf8("txt_news"))
+        self.cmb_newslist = QtGui.QComboBox(self.tab_4)
+        self.cmb_newslist.setGeometry(QtCore.QRect(639, 132, 155, 20))
+        self.cmb_newslist.setStyleSheet(_fromUtf8(" QComboBox {\n"
+"     border: 1px solid rgb(121,121,121);\n"
+"     border-radius: 3px;\n"
+"     padding: 1px 18px 1px 3px;\n"
+"     min-width: 6em;\n"
+" }\n"
+"\n"
+" QComboBox:editable {\n"
+"     background: white;\n"
+" }\n"
+"\n"
+" QComboBox:!editable, QComboBox::drop-down:editable {\n"
+"      background: white;\n"
+" }\n"
+"\n"
+" /* QComboBox gets the \"on\" state when the popup is open */\n"
+" QComboBox:!editable:on, QComboBox::drop-down:editable:on {\n"
+"     background: white;\n"
+" }\n"
+"\n"
+" QComboBox:on { /* shift the text when the popup opens */\n"
+"     padding-top: 3px;\n"
+"     padding-left: 4px;\n"
+" }\n"
+"\n"
+" QComboBox::drop-down {\n"
+"     subcontrol-origin: padding;\n"
+"     subcontrol-position: top right;\n"
+"     width: 15px;\n"
+"\n"
+"     border-left-width: 1px;\n"
+"     border-left-color: darkgray;\n"
+"     border-left-style: solid; /* just a single line */\n"
+"     border-top-right-radius: 3px; /* same radius as the QComboBox */\n"
+"     border-bottom-right-radius: 3px;\n"
+" }\n"
+"\n"
+" QComboBox::down-arrow {\n"
+"     image: url(/ui/down-arrow.png);\n"
+" }\n"
+"\n"
+" QComboBox::down-arrow:on { /* shift the arrow when popup is open */\n"
+"     top: 1px;\n"
+"     left: 1px;\n"
+" }\n"
+"\n"
+" QComboBox QAbstractItemView {\n"
+"     border: 1px solid rgb(26,26,26);\n"
+"     selection-background-color: rgb(176,176,176);\n"
+"    color:rgb(26,26,26)\n"
+" }"))
+        self.cmb_newslist.setFrame(False)
+        self.cmb_newslist.setObjectName(_fromUtf8("cmb_newslist"))
+        self.tabWidget.addTab(self.tab_4, _fromUtf8(""))
         self.tab_2 = QtGui.QWidget()
         self.tab_2.setObjectName(_fromUtf8("tab_2"))
         self.lst_queue = QtGui.QTreeWidget(self.tab_2)
-        self.lst_queue.setGeometry(QtCore.QRect(-1, -1, 797, 100))
+        self.lst_queue.setGeometry(QtCore.QRect(-1, -1, 797, 122))
         self.lst_queue.setStyleSheet(_fromUtf8("QTreeWidget {\n"
 "    show-decoration-selected: 1;\n"
 "    border: 4px solid white;\n"
@@ -782,7 +905,7 @@ class Ui_MainWindow(object):
         self.lst_queue.header().setDefaultSectionSize(100)
         self.lst_queue.header().setMinimumSectionSize(100)
         self.btn_clear_downloads = QtGui.QPushButton(self.tab_2)
-        self.btn_clear_downloads.setGeometry(QtCore.QRect(-1, 100, 121, 23))
+        self.btn_clear_downloads.setGeometry(QtCore.QRect(0, 123, 121, 23))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Segoe UI"))
         self.btn_clear_downloads.setFont(font)
@@ -821,7 +944,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(2)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -834,12 +957,13 @@ class Ui_MainWindow(object):
         self.lst_files.headerItem().setText(3, QtGui.QApplication.translate("MainWindow", "| NODE", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("MainWindow", "FILES", None, QtGui.QApplication.UnicodeUTF8))
         self.btn_grab.setText(QtGui.QApplication.translate("MainWindow", "DOWNLOAD", None, QtGui.QApplication.UnicodeUTF8))
-        self.btn_connect.setText(QtGui.QApplication.translate("MainWindow", "CONNECT", None, QtGui.QApplication.UnicodeUTF8))
-        self.lne_ip.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "IP:PORT", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_connect.setText(QtGui.QApplication.translate("MainWindow", "PROFILES", None, QtGui.QApplication.UnicodeUTF8))
+        self.lne_filter.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "FILTER", None, QtGui.QApplication.UnicodeUTF8))
         self.lab_downloaded_files.setText(QtGui.QApplication.translate("MainWindow", "Downloaded Files:", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setText(QtGui.QApplication.translate("MainWindow", "Log", None, QtGui.QApplication.UnicodeUTF8))
         self.lab_connected_nodes.setText(QtGui.QApplication.translate("MainWindow", "Connected Nodes: ", None, QtGui.QApplication.UnicodeUTF8))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("MainWindow", "INFO", None, QtGui.QApplication.UnicodeUTF8))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QtGui.QApplication.translate("MainWindow", "NEWS", None, QtGui.QApplication.UnicodeUTF8))
         self.lst_queue.headerItem().setText(0, QtGui.QApplication.translate("MainWindow", "| FILE", None, QtGui.QApplication.UnicodeUTF8))
         self.lst_queue.headerItem().setText(1, QtGui.QApplication.translate("MainWindow", "| PROGRESS", None, QtGui.QApplication.UnicodeUTF8))
         self.lst_queue.headerItem().setText(2, QtGui.QApplication.translate("MainWindow", "| SIZE", None, QtGui.QApplication.UnicodeUTF8))
